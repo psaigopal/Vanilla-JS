57fcda2efcc9bfaa4d841a099f817d1ae61b80d5
saigopal <saigopalpekal@gmail.com>
2020-09-14 21:59:19 +0530

    Initial Commit: Printing to Console.

---
+++ 001-hellojs.js
@@ -0,0 +1,7 @@
+console.log('Hello JavaScript!!!');
+console.log('Plain JavaScript is referred as Vanilla JavaScript');
+console.log('Semicolon is optional')
+console.log("We can declare a string using double quotes also");
+console.log("JavaScript is the language of Modern Web Development");
+console.log("JavaScript provides interactiveness to plain old HTML page")
+console.log('So learn JavaScript and have fun')
